85772739a5a00035f565a4da32231b5da0232f4c
xiaolai <3419199258@qq.com>
2026-07-18 12:11:22 +0800

    补充docker文件等等

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,6 +1,10 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.apache.catalina.User;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
@@ -8,5 +12,21 @@ public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLogLevel() {
         return Logger.Level.FULL;
+    }
+
+    // feign请求拦截器
+    @Bean
+    public RequestInterceptor feignRequestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                // 设置线程变量中的用户Id
+                Long userId = UserContext.getUser();
+                // 设置到feign请求头
+                if (userId != null) {
+                    template.header("user-info", userId.toString());
+                }
+            }
+        };
     }
 }
